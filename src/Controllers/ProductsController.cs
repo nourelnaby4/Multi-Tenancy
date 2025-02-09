2aea401dc353708a3e7e7bc5ca89db0d924b5bae
AhmedNourElnabi <91994457+nourelnaby4@users.noreply.github.com>
2025-02-09 06:44:01 +0200

    feate: add product service and controllers

---
+++ src/Controllers/ProductsController.cs
@@ -0,0 +1,49 @@
+﻿using API.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly IProductService _productService;
+
+    public ProductsController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Product>>> GetAll()
+    {
+        var products = await _productService.GetAllAsync();
+        return Ok(products);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Product>> GetById(int id)
+    {
+        var product = await _productService.GetByIdAsync(id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+        return Ok(product);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Product>> Create([FromBody] CreateProductDto dto)
+    {
+        if (dto == null)
+        {
+            return BadRequest("Product cannot be null");
+        }
+
+
+        var product = new Product { Name = dto.Name, Description = dto.Description ,Rate= dto.Rate };
+
+        var createdProduct = await _productService.CreateAsync(product);
+        return CreatedAtAction(nameof(GetById), new { id = product.Id }, product);
+    }
+}
---
+++ src/Services/IProductService.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Services;
+
+public interface IProductService
+{
+    Task<Product> CreateAsync(Product product);
+    Task<IReadOnlyList<Product>> GetAllAsync();
+    Task<Product?> GetByIdAsync (int id);    
+}
